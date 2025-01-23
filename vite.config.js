0c933c5df155eab89279fcd72afbf690b758ff07
Lautaro Muiño <lautaromuino2003@gmail.com>
2025-01-22 21:03:55 -0300

    mas cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio/',
+  base: '/react-portfolio/', // Mantén el nombre del repositorio
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: './assets/[name]-[hash][extname]',
+        chunkFileNames: './assets/[name]-[hash].js',
+        entryFileNames: './assets/[name]-[hash].js',
+      },
+    },
+  },
+});
